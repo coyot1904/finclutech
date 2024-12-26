cae8169a8cee66d3a8cdc3c447149d861eba435e
coyot1904 <coyot1904@gmail.com>
2024-12-26 21:23:12 +0300

    Initial Commit

---
+++ finclutech/finclutechApp.swift
@@ -0,0 +1,17 @@
+//
+//  finclutechApp.swift
+//  finclutech
+//
+//  Created by keyvan mozaffari on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct finclutechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
