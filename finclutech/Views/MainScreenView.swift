5f585ef366062d08a271c37288202e28d361416a
coyot1904 <coyot1904@gmail.com>
2024-12-29 11:15:31 +0300

    first commit

---
+++ finclutech/Views/MainScreenView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text("Welcome to the Main Screen!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct MainScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreenView()
+    }
+}
